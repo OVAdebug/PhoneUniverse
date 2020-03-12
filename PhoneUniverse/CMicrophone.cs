f074e301164b907e2d9b53473e1d407addc3798a
OVA <OVA@DK01WV2281>
2020-03-12 11:51:02 +0200

    Add project files.

---
+++ PhoneUniverse/CMicrophone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneUniverse
+{    
+    class CMicrophone
+    {        
+        private double lowFreq;
+        private double uperFreq;
+        private double bitrate;
+        public override string ToString()
+        {
+            return string.Format("equipment exists");
+        }
+    }
+}
---
+++ PhoneUniverse/CSimCard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneUniverse
+{
+    public enum SimCardType
+    {
+        PLAIN,MINI,MICRO
+    }
+ 
+    public interface ISimCard
+    {
+        int cardlots{get;}
+         List<SimCardType> simCards { get; }
+    }
+
+    public abstract class CSimpleSimCard
+    {
+        
+        protected SimCardType cardType;
+        public abstract void Display(ISimCard cards);
+                
+    }
+    public class CSimCard : CSimpleSimCard
+    {
+        public CSimCard()
+        {
+
+            simCards = new List<SimCardType>();
+        }
+        public CSimCard(int n)
+        {
+
+            simCards = new List<SimCardType>();
+            for (int i=0;i<n;i++)
+                simCards.Add(SimCardType.MICRO);
+
+        }
+        public override  void Display(ISimCard cards)
+        {
+            
+        }
+        public override string ToString()
+        {
+            string s=string.Format("Number of cards supported: {0}", cardlots);
+            return s;
+        }
+
+        public int cardlots { get { return simCards.Count; } }
+        public List<SimCardType> simCards { get; }
+    }
+}
